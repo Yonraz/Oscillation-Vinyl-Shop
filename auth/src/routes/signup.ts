5b3897e32a19c778bff513b9047ed41d62e1d114
yonraz <yraz530@gmail.com>
2024-04-17 15:56:12 +0300

    update signup

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = Router();
 
@@ -12,10 +13,10 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
     console.log("Creating a user...");
